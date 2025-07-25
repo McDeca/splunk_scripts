6675253d54b716590c8ac6b944721559ec399ee1
McDeca <55005692+McDeca@users.noreply.github.com>
2025-07-25 18:46:53 +0200

    Update and rename install_splunk.sh to install_hf.sh

--- install_hf.sh
+++ install_hf.sh
@@ -1,3 +1,5 @@
+### M.Steinmetz @ 07/2025 ###
+
 #!/bin/bash
 
 # 1. Anlegen des Download-Ordners im Home-Verzeichnis
@@ -22,10 +24,6 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
-# 4. Entpacken nach /opt
-echo "[INFO] Entpacke $FILENAME nach /opt ..."
-sudo tar -xzvC /opt -f "$FILENAME"
-
 # 5. Bestimmen ob Forwarder oder Indexer
 if [[ "$FILENAME" == splunkforwarder-* ]]; then
   SPLUNK_DIR="/opt/splunkforwarder"
@@ -33,20 +31,16 @@ else
   SPLUNK_DIR="/opt/splunk"
 fi
 
-# Verzeichnis sicherstellen
-if [ ! -d "$SPLUNK_DIR" ]; then
-  echo "[INFO] Erstelle Verzeichnis $SPLUNK_DIR ..."
-  sudo mkdir -p "$SPLUNK_DIR"
-else
-  echo "[INFO] Verzeichnis $SPLUNK_DIR existiert bereits."
-fi
+# 4. Entpacken nach /opt
+echo "[INFO] Entpacke $FILENAME nach /opt ..."
+sudo tar -xzvC /opt -f "$FILENAME"
 
 # 6. Benutzer für Berechtigung abfragen
-read -p "Für welchen Benutzer sollen die Rechte auf $SPLUNK_DIR gesetzt werden? " SPLUNK_USER
+read -p "Welchen Service-User sollen auf $SPLUNK_DIR berechtigt werden? " SPLUNK_USER
 
 # 7. Berechtigungen setzen
 echo "[INFO] Setze Besitzerrechte auf $SPLUNK_USER"
-sudo chown -R "$SPLUNK_USER" "$SPLUNK_DIR"
+sudo chown -R "$SPLUNK_USER:$SPLUNK_USER" "$SPLUNK_DIR"
 
 # 8. Splunk starten (inkl. Lizenzannahme)
 echo "[INFO] Starte Splunk (interaktive Lizenzannahme)..."
@@ -57,6 +51,11 @@ if [ $? -ne 0 ]; then
   echo "[FEHLER] Splunk konnte nicht erfolgreich gestartet werden. Vorgang abgebrochen."
   exit 1
 fi
+
+#  Berechtigungen erneut setzen
+echo "[INFO] Setze Besitzerrechte auf $SPLUNK_USER"
+sudo chown -R "$SPLUNK_USER:$SPLUNK_USER" "$SPLUNK_DIR"
+
 # 9. Servernamen abfragen
 read -p "Bitte den gewünschten Servernamen eingeben: " SERVERNAME
 
@@ -64,6 +63,10 @@ read -p "Bitte den gewünschten Servernamen eingeben: " SERVERNAME
 echo "[INFO] Setze Servernamen und Default-Hostname..."
 "$SPLUNK_DIR/bin/splunk" set servername "$SERVERNAME"
 "$SPLUNK_DIR/bin/splunk" set default-hostname "$SERVERNAME"
+
+# 13. Splunk für Autostart konfigurieren
+echo "[INFO] Aktiviere Splunk beim Systemstart für Benutzer '$SPLUNK_USER' ..."
+sudo "$SPLUNK_DIR/bin/splunk" enable boot-start -user "$SPLUNK_USER"
 
 # 11. IP des Deploymentservers abfragen
 read -p "Bitte die IP des Deploymentservers eingeben (nur IP, ohne Port): " DEPLOY_IP
@@ -73,8 +76,22 @@ DEPLOY_TARGET="${DEPLOY_IP}:8089"
 echo "[INFO] Setze Deployment-Server auf $DEPLOY_TARGET"
 "$SPLUNK_DIR/bin/splunk" set deploy-poll "$DEPLOY_TARGET"
 
-# 13. Splunk für Autostart konfigurieren
-echo "[INFO] Aktiviere Splunk beim Systemstart für Benutzer $SPLUNK_USER ..."
-sudo "$SPLUNK_DIR/bin/splunk" enable boot-start -user "$SPLUNK_USER"
+echo "[FERTIG] Splunk wurde erfolgreich installiert & gestartet."
 
-echo "[FERTIG] Splunk wurde erfolgreich installiert, gestartet und konfiguriert."
+echo "[RESTART] Splunk wird neugestartet."
+# Restart Splunkd
+"$SPLUNK_DIR/bin/splunk" restart
+
+# 6. splunkforwarder aktivieren
+echo "[INFO] SplunkForwarder wird aktiviert!"
+read -p "Splunk Admin User: " SPLUNK_ADMIN
+read -p "Splunk Admin User Password: " SPLUNK_ADMIN_PW
+"$SPLUNK_DIR/bin/splunk" enable app SplunkForwarder -auth $SPLUNK_ADMIN:$SPLUNK_ADMIN_PW
+echo "[FERTIG] SplunkForwarder ist aktiviert!"
+
+#
+echo "[INFO] Zu welchem Server soll weitergeleitet werden?"
+read -p "Add forward-server to indexers ip? IP: " INDEXER_IP
+"$SPLUNK_DIR/bin/splunk" add forward-server INDEXER_IP:8089 -auth $SPLUNK_ADMIN:$SPLUNK_ADMIN_PW
+
+echo "[INFO] SplunkForwarder sendet von Daten an INDEXER_IP:8089 ist aktiviert!"
